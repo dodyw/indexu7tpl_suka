3d833053c0c79e2be6e459af6fbbde067c450e56
dody <dodyrw@gmail.com>
2011-11-14 05:47:32 +0700

    Remove old files

--- html/wrapper.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  function html_wrapper($html, $param) {
-    $html = "<p>$html</p>";
-    ($param['show_title']) ? $html = "<h3>"._t($param['title'])."</h3>$html" : false;
-    $html = "<div class=\"post\">$html</div>";
-    
-    return $html;
-  } 
-?>
